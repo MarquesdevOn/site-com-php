c533f5d513e6e8d54323f83fac260c1a00cc12f6
MarquesdevOn <thalitajasse@gmail.com>
2024-10-30 14:21:10 -0300

    Criação da página equipe

---
+++ equipe.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Equipe - Site</title>
+</head>
+<body>
+    <header>
+        <h1>Site Exemplo</h1>
+
+        <!-- fazer menu -->
+        <nav> 
+            <a href="">Home</a>
+            <a href="">Equipe</a>
+            <a href="">Serviços</a>
+        </nav>
+    </header>
+
+    <main>
+        <article>
+            <h2>Nossa equipe</h2>
+            <ul>
+                <li>Bob Esponja</li>
+                <li>Lula Moluco</li>
+                <li>Patrick Estrla</li>
+                <li>Sandy Bochechas</li>
+                <li>Seu Siriguejo</li>
+            </ul>
+        </article>
+    </main>
+
+    <footer>
+        <h2>Site criado como exemplo</h2>
+    </footer>
+</body>
+</html>
---
+++ servicos.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site</title>
+</head>
+<body>
+    <header>
+        <h1>Site Exemplo</h1>
+
+        <!-- fazer menu -->
+        <nav> 
+            <a href="">Home</a>
+            <a href="">Equipe</a>
+            <a href="">Serviços</a>
+        </nav>
+    </header>
+
+    <main>
+        <article>
+            <h2>Bem-vindo(a)</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, rem.</p>
+        </article>
+    </main>
+
+    <footer>
+        <h2>Site criado como exemplo</h2>
+    </footer>
+</body>
+</html>
